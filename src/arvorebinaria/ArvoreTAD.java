eb9956b389e5edfcce54330ebf07c4a1b145accb
EdelbertoRosler <edelbertorosler@gmail.com>
2021-07-17 00:39:00 -0300

    Implementando alguns métodos

--- src/arvorebinaria/ArvoreTAD.java
+++ src/arvorebinaria/ArvoreTAD.java
@@ -2,49 +2,106 @@ package arvorebinaria;
 
 public class ArvoreTAD {
     private Nodo raiz;
+    private int num;//variável para contar a quantidade de elementos
 
     public ArvoreTAD() {
         this.raiz = null;
+        this.num = 0;
     }
 
     public int tamanho() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+        return this.num;
+    }//quantidade de elementos
 
     public boolean ehVazia() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return this.num == 0;
+    }//true se a árvore está vazia
+
+    public int acessaMenor(){//chama o método recursivo
+        return this.acessaMenorRec(this.raiz);
+    }//encontra o menor valor
+
+    private int acessaMenorRec(Nodo n) {
+        if(n == null || n.esq == null){
+            return n.elem;//ponto de parada
+        }
+        return this.acessaMenorRec(n.esq);//vai indo para a esquerda recursivamente
     }
 
-    public int acessaMenor() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+    public int acessaMaior(){//chama o método recursivo
+        return this.acessaMaiorRec(this.raiz);
+    }//encontra o maior valor
 
-    public int acessaMaior() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    private int acessaMaiorRec(Nodo n) {
+        if(n == null || n.dir == null){
+            return n.elem;//ponto de parada
+        }
+        return this.acessaMaiorRec(n.dir);//vai indo para a direita recursivamente
     }
 
     public void insere(int elem) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+        if (raiz == null){
+            this.raiz = new Nodo(elem);
+        }
+        else {
+            this.insereRec(elem, raiz);
+        }
+        this.num += 1;
+
+    }//insere um elemento
 
     private void insereRec(int elem, Nodo n) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (elem == n.elem){
+            System.out.println("O elemento já está na árvore");
+        }
+        else if (elem < n.elem){
+            if (n.esq == null){
+                n.esq = new Nodo(elem);
+            }
+            else{
+                this.insereRec(elem,n.esq);
+            }
+        }
+        else if (elem > n.elem){
+            if (n.dir == null){
+                n.dir = new Nodo(elem);
+            }
+            else {
+                this.insereRec(elem, n.dir);
+            }
+        }
     }
 
     public boolean pesquisa(int elem) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+        return  this.pesquisaRec(elem, raiz);
+    }//pesquisa por um elemento
 
     private boolean pesquisaRec(int elem, Nodo n) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(n == null){
+            return  false;
+        }
+        if(elem == n.elem){
+            return  true;
+        }
+        if (elem < n.elem){
+            return pesquisaRec(elem, n.esq);
+        }
+        else {
+            return  pesquisaRec(elem, n.dir);
+        }
     }
 
     public void imprimeEmOrdem() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+        this.imprimeEmOrdemRec(this.raiz);
+    }//imprime em ordem crescente, em linhas separadas
 
     private void imprimeEmOrdemRec(Nodo n) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(n == null){
+            return;
+        }
+        this.imprimeEmOrdemRec(n.esq);
+        System.out.println(n.elem);
+        this.imprimeEmOrdemRec(n.dir);
     }
 
     public void imprimePreOrdem() {
@@ -68,7 +125,8 @@ public class ArvoreTAD {
      **************************************************************************/
     public void remove(int elem) {
         this.raiz = this.removeRec(elem, this.raiz);
-    }
+        this.num -= 1;
+    }//remove um elemento
 
     public Nodo removeRec(int elem, Nodo n) {
         if(n == null) {
@@ -104,7 +162,7 @@ public class ArvoreTAD {
 
     public void imprimeFormatoArvore() {
         this.imprimeFormatoArvore(this.raiz, 0);
-    }
+    }//imprime no formato de árvore
 
     private void imprimeFormatoArvore(Nodo n, int nivel) {
         if(n != null) {
--- src/arvorebinaria/Principal.java
+++ src/arvorebinaria/Principal.java
@@ -2,10 +2,27 @@ package arvorebinaria;
 import java.util.Scanner;
 /**
  *
- * @author edel
+ * @author edelberto Rösler
  */
 public class Principal {
     public static void main(String[] args) {
+        ArvoreTAD arv = new ArvoreTAD();
+
+        arv.insere(10);
+        arv.insere(15);
+        arv.insere(8);
+        arv.insere(55);
+        arv.insere(99);
+        arv.insere(81);
+        arv.insere(88);
+        arv.imprimeEmOrdem();
+//        arv.imprimeFormatoArvore();
+        arv.remove(88);
+        System.out.println(arv.pesquisa(81));
+        System.out.println(arv.tamanho());
+        System.out.println(arv.ehVazia());
+        System.out.println(arv.acessaMenor());
+        System.out.println(arv.acessaMaior());
 
 
     }
